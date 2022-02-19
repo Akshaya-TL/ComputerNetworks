e8c31a9461a0cf1fe10974a06a3e6e470c89cacd
Akshaya-TL <72205755+Akshaya-TL@users.noreply.github.com>
2022-02-20 01:09:23 +0530

    optimized code

--- 4_djikstra.c
+++ 4_djikstra.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-#include<math.h>
-#include<limits.h>
+#include <stdio.h>
+#include <math.h>
+#include <limits.h>
 /*
 requirements
 int graph[v][v]
@@ -13,67 +13,68 @@ bool visited[v]
     destns and mark the current src as visited
  3> print all nodes with distances
  */
- 
-  int minDistance(int v, int dist[v], int visited[v]){
-      int mini = INT_MAX, idx;
-      for(int i=0; i<v; i++){
-          if(visited[i]==0 && dist[i] <= mini){
-              mini = dist[i];
-              idx = i;
-          }
-      }
-      return idx;
-  }
- 
-  void printDistances(int v, int dist[v], int src){
-      printf("source  destination  time-taken\n");
-      for(int i=0; i<v; i++){
-         printf(" %d         %d          %d      \n", src, i, dist[i]);
-      }
-      return;
-  }
- 
-  void djikstra(int v, int graph[v][v], int src1){
-      int dist[v], count = 0;
-      int visited[v];
- 
-      for(int i=0; i<v; i++){
-          dist[i] = INT_MAX;
-          visited[i] = 0;
-      }
+
+void printDistances(int v, int dist[v], int src)
+{
+    printf("source  destination  time-taken\n");
+    for (int i = 0; i < v; i++)
+    {
+        printf(" %d         %d          %d      \n", src, i, dist[i]);
+    }
+    return;
+}
+
+void djikstra(int v, int graph[v][v], int src1)
+{
+    int dist[v], count = 0;
+    int visited[v];
+
+    for (int i = 0; i < v; i++)
+    {
+        dist[i] = INT_MAX;
+        visited[i] = 0;
+    }
     dist[src1] = 0;
+    int src = src1;
+    while (count < v)
+    {   
+        visited[src] = 1;
+        count++;
+        int newSrc = 0;
+        for (int i = 0; i < v; i++)
+        {
+            if (graph[src][i] && visited[i] == 0 && dist[src] != INT_MAX && graph[src][i] + dist[src] < dist[i])
+            {
+                dist[i] = graph[src][i] + dist[src];
+                newSrc = i;
+            }
+        }
+        src = newSrc;
+    }
 
-     while(count < v){
-         int src = minDistance(v, dist, visited);
-         visited[src] = 1;
-         count++;
-         for(int i=0; i<v; i++){
-             if(graph[src][i] && visited[i]==0 && dist[src] != INT_MAX && graph[src][i] + dist[src] < dist[i]){
-                 dist[i] = graph[src][i] + dist[src];
-             }
-         }
-     }
+    printDistances(v, dist, src1);
+}
 
-     printDistances(v, dist, src1);
- }
+int main()
+{
+    int v, src = 0;
+    printf("Enter the number of nodes in network: ");
+    scanf("%d", &v);
 
+    int graph[v][v];
+    printf("Enter the time taken to transfer data packets from src to destn:  \n");
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            scanf("%d", &graph[i][j]);
+        }
+    }
 
- int main(){
-     int v, src=0;
-     printf("Enter the number of nodes in network: ");
-     scanf("%d", &v);
+    printf("Enter source node: ");
+    scanf("%d", &src);
+    djikstra(v, graph, src);
 
-     int graph[v][v];
-     printf("Enter the time taken to transfer data packets from src to destn:  \n");
-     for(int i=0; i<v; i++){
-         for(int j=0; j<v; j++){
-             scanf("%d", &graph[i][j]);
-         }
-     }
+    return 0;
+}
 
-     printf("Enter source node: ");
-     scanf("%d", &src);
-     djikstra(v, graph, src);
-
-     return 0;
- }
